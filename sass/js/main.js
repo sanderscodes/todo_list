299bcaf0754d3c2ce8f728d1c4536d42e5aae01d
sanderscodes <ricesoup@yahoo.com>
2024-04-01 21:56:02 -0400

    added more js

--- sass/js/main.js
+++ sass/js/main.js
@@ -8,3 +8,46 @@ document.addEventListener("readystatechange", (event) => {
         initApp();
     }
 });
+
+const initApp = () => {
+    refreshThePage();
+};
+
+const refreshThePage = () => {
+    clearListDisplay();
+    renderList();
+};
+const clearListDisplay = () => {
+    const parentElement = document.getElementById("listItems");
+    deleteContents(parentElement);
+}; 
+const deleteContents = (parentElement) => {
+    let child = parentElement.lastElementChild;
+    while (child) {
+        parentElement.removeChild(child);
+        child = parentElement.lastElementChild;
+    }
+};
+
+const renderList = () => {
+    const list = toDoList.getList();
+    list.forEach((item) => {
+        buildListItem(item);
+    });
+};
+const buildListItem = (item) => {
+    const div = document.createElement("div");
+    div.className = "item";
+    const check = document.createElement("input");
+    check.type = "checkbox";
+    check.id = item.getId();
+    check.tabIndex = 0;
+    // addClickListenerToCheckbox(check);
+    const label = document.createElement("label");
+    label.htmlFor = item.getId();
+    label.textContent = item.getItem();
+    div.appendChild(check);
+    div.appendChild(label);
+    const container = document.getElementById("listItems");
+    container.appendChild(div);
+};
